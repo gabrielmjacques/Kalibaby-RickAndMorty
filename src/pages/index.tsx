46e6409ebdc876d867b3d7692d954bdf5b5b0ad8
gabrielmjacques <egmoreira10@gmail.com>
2023-05-09 13:06:37 -0300

    Correção de responsividade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,18 @@ export default function Home()
 
       <div className={ styles.bg }></div>
 
-      <div className="col-md-5 col-sm-10 mx-auto" style={ { paddingTop: "100px" } }>
+      <div className={ `col-md-5 col-sm-10 mx-auto ${styles.mainCardContainer}` }>
 
         <div className={ `row rounded-5 ${styles.mainCard}` }>
-          <div className="row">
+          <div className="row mx-auto">
 
             <div className="row mx-auto">
               <img className={ `p-5 ${styles.logo}` } src={ "/logo.png" } alt="Rick and Morty logo" width={ "50%" } />
             </div>
             <div className="row mx-auto mt-5">
-              <Link className='btn btn-dark' href="/listpage/1">Ver Personagens</Link>
+              <Link href="/listpage/1">
+                <button className='btn btn-dark w-100'>Ver Personagens</button>
+              </Link>
             </div>
 
           </div>
